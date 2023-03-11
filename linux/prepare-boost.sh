85b715b3ccc231d6fb366f91dd179586057347c3
0xp1n <s3r0s4pi3ns@protonmail.com>
2023-03-11 12:41:52 +0000

    ssh_message_prefix function

--- linux/prepare-boost.sh
+++ linux/prepare-boost.sh
@@ -26,6 +26,12 @@ linux_main() {
 # Make sure you generated the ssh keys and has a privilege user
 ###
 
+ssh_message_prefix() {
+    local content=$1
+
+    echo -e "$yellowColour [ SSH Hardening ]$endColour $content"
+}
+
 apply_sshd_configuration_file() {
     local TARGET_DIR="/etc/sshd_config.d/"
     local SSH_CONFIGURATION_FILE="$CURRENT_DIR/sshd_config/hardening-init.conf"    
@@ -33,13 +39,14 @@ apply_sshd_configuration_file() {
   
     if directory_exists "/etc/sshd_config.d"; then
         while ! port_in_valid_range $SSH_PORT; do 
-            read -rp "$(echo -e "$yellowColour [ SSH Hardening ]$endColour Select a port for OpenSSH service (Default 22) ")" SSH_PORT
+            read -rp "$(ssh_message_prefix "Select a port for OpenSSH service (Default 22) ")" SSH_PORT
              is_empty $SSH_PORT \
                 && SSH_PORT=22
         done
 
+        ssh_message_prefix "Copied$cyanColour $SSH_CONFIGURATION_FILE$grayColour to$endColour $cyanColour$TARGET_DIR$endColour $greenColour [SUCCESS]$endColour"
         cp "$SSH_CONFIGURATION_FILE" "$TARGET_DIR"
     else
-      echo -e "$yellowColour [ SSH Hardening ]$endColour The configuration folder /etc/sshd_config.d does not exists in this system$redColour [FAILED] $endColour"   
+      echo -e "$yellowColour [ SSH Hardening ]$endColour The configuration folder$cyanColour /etc/sshd_config.d$endColour does not exists in this system$redColour [FAILED]$endColour"   
     fi
 }
